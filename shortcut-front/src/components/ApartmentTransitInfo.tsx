8786a146afc3a230abc949ad99674725927ed564
Toni Sallanmaa <toni@sallanmaa.fi>
2019-07-16 14:31:06 +0300

    Popover visualization of itineraries

--- shortcut-front/src/components/ApartmentTransitInfo.tsx
+++ shortcut-front/src/components/ApartmentTransitInfo.tsx
@@ -1,12 +1,12 @@
 import * as React from "react";
-import { Table } from 'reactstrap';
+import { Table, PopoverHeader, PopoverBody, UncontrolledPopover } from 'reactstrap';
 
 export interface ApartmentTransitInfoProps {
     id: string
 }
 
 export interface ApartmentTransitInfoState {
-    data: any;
+    data: any
 }
 
 export class ApartmentTransitInfo extends React.Component<ApartmentTransitInfoProps, ApartmentTransitInfoState> {
@@ -22,7 +22,7 @@ export class ApartmentTransitInfo extends React.Component<ApartmentTransitInfoPr
     componentDidMount() {
         fetch(`${__API__}/api/apartments/${this.props.id}/transit`)
             .then((response) => response.json())
-            .then((res: any[]) => {
+            .then((res: any) => {
                 this.setState({ data: res })
             }); 
     }
@@ -43,7 +43,13 @@ export class ApartmentTransitInfo extends React.Component<ApartmentTransitInfoPr
     }
 
     render() {
-        return (           
+        const itineraries: any = {};
+        this.state.data.summaries.forEach((sum: any) => {
+            itineraries[sum.tag] = this.state.data.itineraries.filter((it: any) => it.tag === sum.tag)[0].itineraries;
+        });
+
+        return (  
+            <div>        
             <Table striped bordered>
                 <thead>
                     <tr>
@@ -57,7 +63,7 @@ export class ApartmentTransitInfo extends React.Component<ApartmentTransitInfoPr
                 </thead>                           
                 <tbody>
                     { this.state.data.summaries.map((sum: any) => (
-                    <tr key={sum.tag}>
+                    <tr key={sum.tag} id={sum.tag}>
                         <th scope="row">{ sum.tag }</th>
                         <td>{ this.formatDuration(sum.summary.averageDuration) }</td>
                         <td>{ this.safeToFixed(sum.summary.averageTransitLegCount, 2) }</td>
@@ -68,6 +74,26 @@ export class ApartmentTransitInfo extends React.Component<ApartmentTransitInfoPr
                     ))}                            
                 </tbody>
             </Table>
+            { this.state.data.summaries.map((sum: any) => (
+            <UncontrolledPopover placement="bottom" trigger="click" target={sum.tag}>
+                <PopoverHeader>Itineraries</PopoverHeader>
+                <PopoverBody>
+                    {
+                        itineraries[sum.tag].map((it: any, index: number) => (
+                            <div style={{marginBottom: "10px"}}>
+                            <div><b>Itinerary {index+1}</b></div>
+                            {
+                                it.legs.map((leg: any, legIndex: number) => (
+                                <div>Leg {legIndex+1}: {leg.mode} {leg.route ? `(${leg.route.shortName})` : ""} {leg.from.name} - {leg.to.name} <b>{this.formatDuration((leg.endTime-leg.startTime)/1000)}</b></div>
+                                ))
+                            }
+                            </div>
+                        ))
+                    }
+                </PopoverBody>
+            </UncontrolledPopover>
+            ))}
+            </div> 
         );
     }
 }
